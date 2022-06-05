eb5ad16eb9abd1a9b637c1ee0baa3ec3ff06ab6a
sharan9678 <sharan9678@gmail.com>
2022-06-05 13:43:14 +0530

    added recursion part 2

---
+++ src/com/sharan/dsa/recursion_part2/Sum.java
@@ -0,0 +1,15 @@
+package com.sharan.dsa.recursion_part2;
+
+public class Sum {
+    public static void main(String[] args) {
+        int ans = sum(5);
+        System.out.println(ans);
+    }
+
+    static int sum(int n) {
+        if (n <= 1) {
+            return 1;
+        }
+        return n + sum(n - 1);
+    }
+}
