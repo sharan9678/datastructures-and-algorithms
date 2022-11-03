bc74922b334411b970674132613808130c050359
sharan9678 <sharan9678@gmail.com>
2022-11-03 07:39:17 +0530

    added doubly linked list

---
+++ src/com/sharan/udemy/doublyLinkedList/Main.java
@@ -0,0 +1,14 @@
+package com.sharan.udemy.doublyLinkedList;
+
+public class Main {
+    public static void main(String[] args) {
+        DoublyLinkedList dll = new DoublyLinkedList();
+        dll.createDLL(1);
+        dll.insertDLL(2, 0);
+        dll.insertDLL(3, 1);
+        dll.insertDLL(4, 7);
+        dll.traverseDLL();
+        dll.reverseTraverseDLL();
+        dll.searchNode(3);
+    }
+}
